550f018320b9d8969b1187dac397ceeabb1382b4
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-14 23:00:17 +0900

    モックを表示した

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,3 +1,47 @@
+import Card from '@/stories/Card'
+import { Page } from '@/type/page/page'
+import dateToStr from '@/util/dateToStr'
+import Link from 'next/link'
+
 export default function Home() {
-    return <div></div>
+    const mockData: Page[] = [
+        {
+            id: '9b548af9-e899-4002-8202-613be38bceac',
+            title: 'Notionで書いたブログをウェブページとして公開してみた【Next.js】',
+            tags: [{ name: 'プログラミング', color: 'blue' }],
+            lastEditedTime: '2023-08-11T07:10:00.000Z',
+            slug: 'notion-blog-nextjs',
+            status: 'writing',
+            publishDate: null,
+            blocks: [],
+        },
+        {
+            id: 'f803988f-6182-4d9b-a688-37a463669ef0',
+            title: 'テストページ',
+            tags: [{ name: 'プログラミング', color: 'blue' }],
+            lastEditedTime: '2023-08-11T02:00:00.000Z',
+            slug: 'test',
+            status: 'published',
+            publishDate: '2023-08-11',
+            blocks: [],
+        },
+    ]
+    return (
+        <main>
+            <div className='grid gap-4 p-4'>
+                {mockData.map((page, i) => {
+                    return (
+                        <Link href={`/blog/${page.slug}`} key={i}>
+                            <Card>
+                                <h2>{page.title}</h2>
+                                <p>
+                                    {dateToStr(new Date(page.lastEditedTime))}
+                                </p>
+                            </Card>
+                        </Link>
+                    )
+                })}
+            </div>
+        </main>
+    )
 }
