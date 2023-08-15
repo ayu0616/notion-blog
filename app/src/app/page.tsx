be7ea2cbc499bac42bc429d14e6261ec1d53b0df
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-15 10:29:08 +0900

    ページの画像を表示するように変更した

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -14,6 +14,9 @@ export default function Home() {
             status: 'writing',
             publishDate: null,
             blocks: [],
+            image: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/1d037251-36fe-4636-9f55-87e282a39f7b/Nextjs-logo.svg.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230815%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230815T000841Z&X-Amz-Expires=3600&X-Amz-Signature=24a7103a6f8b25f068835d724e496b9522d93525d1c811720224d619184c2d00&X-Amz-SignedHeaders=host&x-id=GetObject',
+            description:
+                'Notionのページをブログとして公開したいと思ったのでNext.jsを利用して自作することにしました',
         },
         {
             id: 'f803988f-6182-4d9b-a688-37a463669ef0',
@@ -24,6 +27,9 @@ export default function Home() {
             status: 'published',
             publishDate: '2023-08-11',
             blocks: [],
+            image: 'https://4.bp.blogspot.com/-NpzSXawlkuQ/WaPwHY3hy_I/AAAAAAABGR8/VjeNZ-J7RdYJmhcEecLUY9q9_GMERe0NACLcBGAs/s800/test_print_happy_schoolgirl.png',
+            description:
+                'これはテストです。これはテストです。これはテストです。これはテストです。これはテストです。これはテストです。これはテストです。',
         },
     ]
     return (
@@ -32,8 +38,9 @@ export default function Home() {
                 {mockData.map((page, i) => {
                     return (
                         <Link href={`/blog/${page.slug}`} key={i}>
-                            <Card>
-                                <h2>{page.title}</h2>
+                            <Card imgSrc={page?.image ?? ''} horizontal>
+                                <h3>{page.title}</h3>
+                                <p>{page.description}</p>
                                 <p>
                                     {dateToStr(new Date(page.lastEditedTime))}
                                 </p>
