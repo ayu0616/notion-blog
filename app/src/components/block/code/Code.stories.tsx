dea1b8c427f05fc59823b0eb66752702659aa98f
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-16 21:11:56 +0900

    コードブロックの見た目を作成した

--- app/src/components/block/code/Code.stories.tsx
+++ app/src/components/block/code/Code.stories.tsx
@@ -20,5 +20,8 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-    args: {},
+    args: {
+        children: "int n = 0;\ncout << n << endl;",
+        language: 'cpp',
+    },
 }
--- app/src/components/block/code/Code.tsx
+++ app/src/components/block/code/Code.tsx
@@ -1,7 +1,31 @@
-interface CodeProps {}
+import { CSSProperties } from 'react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import './Code.style.scss'
 
-const Code = ({ ...props }: CodeProps) => {
-    return <div></div>
+interface CodeProps {
+    children: string
+    language: string
+    showLineNumbers?: boolean
+    style?: { [key: string]: CSSProperties }
+}
+
+const Code = ({
+    style = vscDarkPlus,
+    showLineNumbers = true,
+    ...props
+}: CodeProps) => {
+    // TODO: コードをコピーする機能を実装する
+    return (
+        <div className='code-block-container'>
+            <div className='absolute left-0 top-0 w-full border-b border-b-gray-300 p-2 text-right text-sm text-white'>
+                <button className='hover:text-yellow-300 hover:underline active:text-yellow-400'>
+                    copy
+                </button>
+            </div>
+            <SyntaxHighlighter {...props} style={style}></SyntaxHighlighter>
+        </div>
+    )
 }
 
 export default Code
