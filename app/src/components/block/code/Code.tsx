717a61faf3f620e029e82d248a4901aa3293e733
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-17 11:29:35 +0900

    コピーしたことを通知するポップアップを作成した

--- app/src/components/block/code/Code.tsx
+++ app/src/components/block/code/Code.tsx
@@ -1,6 +1,7 @@
-import { CSSProperties } from 'react'
+import { CSSProperties, useEffect, useRef, useState } from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import Tooltip from '../../common/tooltip/Tooltip'
 import './Code.style.scss'
 
 interface CodeProps {
@@ -16,24 +17,45 @@ const Code = ({
     showLineNumbers = true,
     ...props
 }: CodeProps) => {
-    // TODO: コピーしたことを通知するポップアップを作成する
+    const buttonRef = useRef<HTMLButtonElement>(null)
+    const [buttonElem, setButtonElem] = useState<HTMLButtonElement | null>(null)
+    useEffect(() => {
+        setButtonElem(buttonRef.current)
+    }, [buttonRef])
+    const [tooltipShow, setTooltipShow] = useState(false)
     const copyCode = () => {
         navigator.clipboard.writeText(props.children)
+        setTooltipShow(true)
+        setTimeout(() => {
+            setTooltipShow(false)
+        }, 1000)
     }
     return (
-        <div className='code-block-container relative'>
-            <div className='absolute left-0 top-0 w-full border-b border-b-gray-300 p-2 text-right text-sm text-white'>
+        <div
+            className='code-block-container rounded-md'
+            style={{ background: style['pre[class*="language-"]'].background }}
+        >
+            <div className='w-full border-b border-b-gray-300 p-2 text-right text-sm text-white'>
                 <button
-                    className='hover:text-yellow-300 hover:underline active:text-yellow-400 p-1'
+                    ref={buttonRef}
+                    className='p-1 hover:text-yellow-300 hover:underline active:text-yellow-400'
                     onClick={copyCode}
                 >
                     copy
                 </button>
+                <Tooltip
+                    isShow={tooltipShow}
+                    elem={buttonElem}
+                    placement='left'
+                >
+                    コードがコピーされました！
+                </Tooltip>
             </div>
             <SyntaxHighlighter
                 {...props}
                 showLineNumbers={showLineNumbers}
                 style={style}
+                customStyle={{ margin: 0 }}
             ></SyntaxHighlighter>
         </div>
     )
