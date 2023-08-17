fc01e45c8a65852f1a0b47b7263221414bd6d853
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-17 18:23:06 +0900

    コールアウトコンポーネントを作成した

---
+++ app/src/components/block/callout/Callout.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import BulletedList from '../bulletedList/BulletedList'
+import BulletedListItem from '../bulletedList/BulletedListItem'
+import Callout from './Callout'
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+    title: 'Example/Callout',
+    component: Callout,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'centered',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {},
+} satisfies Meta<typeof Callout>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+        content: 'Hello World',
+        icon: '👋',
+        children: (
+            <BulletedList>
+                <BulletedListItem>hoge</BulletedListItem>
+                <BulletedListItem>
+                    fuga
+                    <BulletedList>
+                        <BulletedListItem>fuga 1</BulletedListItem>
+                        <BulletedListItem>fuga 2</BulletedListItem>
+                    </BulletedList>
+                </BulletedListItem>
+            </BulletedList>
+        ),
+    },
+}
