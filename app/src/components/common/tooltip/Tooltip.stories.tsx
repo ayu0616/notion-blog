d67862f171317285ff96adedd6be4c215af4aceb
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-17 10:15:16 +0900

    ツールチップを作成した

---
+++ app/src/components/common/tooltip/Tooltip.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { useEffect, useRef, useState } from 'react'
+import { Button } from '../Button'
+import Tooltip from './Tooltip'
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+    title: 'Example/Tooltip',
+    component: Tooltip,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'centered',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {},
+} satisfies Meta<typeof Tooltip>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {
+        children: 'This is a Tooltip',
+    },
+    render: (args) => {
+        const buttonRef = useRef<HTMLButtonElement>(null)
+        const [buttonElem, setButtonElem] = useState<HTMLButtonElement | null>(null)
+        const [show, setShow] = useState(false)
+        useEffect(() => {
+            setButtonElem(buttonRef.current)
+        }, [])
+        const toggleShow = () => {
+            setShow((prev) => !prev)
+        }
+        return (
+            <>
+                <Button className='' ref={buttonRef} onClick={toggleShow}>
+                    これはボタンです
+                </Button>
+                <Tooltip {...args} elem={buttonElem} isShow={show} />
+            </>
+        )
+    },
+}
