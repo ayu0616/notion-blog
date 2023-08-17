dbe713aad4dec92d89a11f5518d05cf2babfc011
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-18 01:40:39 +0900

    Blockコンポーネントが一旦完成した

--- app/src/components/block/Block.tsx
+++ app/src/components/block/Block.tsx
@@ -2,8 +2,17 @@ import Paragraph from '@/components/block/Paragraph'
 import Heading from '@/components/block/heading/Heading'
 import { Block as BlockData } from '@/type/page/block/block'
 import { RichText as RichTextData } from '@/type/page/block/richText'
+import BookMark from './BookMark'
+import Equation from './Equation'
 import RichText from './RichText'
+import BulletedList from './bulletedList/BulletedList'
+import BulletedListItem from './bulletedList/BulletedListItem'
 import Callout from './callout/Callout'
+import Code from './code/Code'
+import Divider from './divider/Divider'
+import Image from './image/Image'
+import NumberedList from './numberedList/NumberedList'
+import NumberedListItem from './numberedList/NumberedListItem'
 
 interface BlockProps {
     data: BlockData
@@ -28,6 +37,9 @@ const RichTexts = ({ datas }: { datas?: RichTextData[] | null }) => {
         </>
     )
 }
+
+const richToText = (data: RichTextData[]) =>
+    data.reduce((acc, cur) => acc + cur.text, '')
 
 const Block = ({ data, ...props }: BlockProps) => {
     switch (data.type) {
@@ -69,6 +81,59 @@ const Block = ({ data, ...props }: BlockProps) => {
                     <Children datas={data.children} />
                 </Callout>
             )
+        case 'bulleted_list':
+            return (
+                <BulletedList>
+                    {data.listItems.map((listItem, i) => (
+                        <Block key={i} data={listItem} />
+                    ))}
+                </BulletedList>
+            )
+        case 'bulleted_list_item':
+            return (
+                <BulletedListItem>
+                    <RichTexts datas={data.richTexts} />
+                    <Children datas={data.children} />
+                </BulletedListItem>
+            )
+        case 'numbered_list':
+            return (
+                <NumberedList>
+                    {data.listItems.map((listItem, i) => (
+                        <Block key={i} data={listItem} />
+                    ))}
+                </NumberedList>
+            )
+        case 'numbered_list_item':
+            return (
+                <NumberedListItem>
+                    <RichTexts datas={data.richTexts} />
+                    <Children datas={data.children} />
+                </NumberedListItem>
+            )
+        case 'bookmark':
+            return <BookMark href={data.url}></BookMark>
+        case 'code':
+            return (
+                <Code language={data.language}>
+                    {richToText(data.richTexts)}
+                </Code>
+            )
+        case 'divider':
+            return <Divider />
+        case 'equation':
+            return <Equation math={data.expression} />
+        case 'image':
+            const alt = (() => {
+                if (data.caption.length) {
+                    return richToText(data.caption) //キャプションがあればキャプションをaltにする
+                } else {
+                    return data.url //なければ画像のURLをaltにする
+                }
+            })()
+            return <Image src={data.url} alt={alt}></Image>
+        default:
+            return <></>
     }
 }
 
