06db59e57d5cfa28fab84bdd72728e33d3901185
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-18 13:41:10 +0900

    gapを受け取れるように変更した

--- app/src/components/common/tag/TagList.tsx
+++ app/src/components/common/tag/TagList.tsx
@@ -3,11 +3,27 @@ import Tag, { TagProps } from './Tag'
 interface TagListProps {
     tagData: TagProps[]
     className?: string
+    gap?: number
 }
 
-const TagList = ({ tagData, className = '', ...props }: TagListProps) => {
+const TagList = ({
+    tagData,
+    className = '',
+    gap = 1 | 2 | 3 | 4,
+    ...props
+}: TagListProps) => {
+    const gapClass =
+        gap === 1
+            ? 'gap-1'
+            : gap === 2
+            ? 'gap-2'
+            : gap === 3
+            ? 'gap-3'
+            : gap === 4
+            ? 'gap-4'
+            : 'gap-3'
     return (
-        <ul className={['flex flex-wrap gap-3', className].join(' ')}>
+        <ul className={['flex flex-wrap', gapClass, className].join(' ')}>
             {tagData.map((tag, index) => (
                 <li key={index}>
                     <Tag {...tag} />
