5e415fb6563e39f5645c315e90fec41ad343b07d
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-18 15:42:36 +0900

    テキストも色をつけるように変更した

--- app/src/components/common/tag/Tag.tsx
+++ app/src/components/common/tag/Tag.tsx
@@ -9,28 +9,28 @@ const Tag = ({ name, color, ...props }: TagProps) => {
     const bg = (() => {
         switch (color) {
             case 'red':
-                return 'bg-red-300/50'
+                return 'bg-red-300/50 text-red-500'
             case 'blue':
-                return 'bg-blue-300/50'
+                return 'bg-blue-300/50 text-blue-500'
             case 'green':
-                return 'bg-green-400/50'
+                return 'bg-green-400/50 text-green-500'
             case 'yellow':
-                return 'bg-yellow-300/50'
+                return 'bg-yellow-300/50 text-yellow-500'
             case 'purple':
-                return 'bg-purple-300/50'
+                return 'bg-purple-300/50 text-purple-500'
             case 'pink':
-                return 'bg-pink-300/50'
+                return 'bg-pink-300/50 text-pink-500'
             case 'gray':
-                return 'bg-gray-300/50'
+                return 'bg-gray-300/50 text-gray-500'
             case 'brown':
-                return 'bg-amber-700/50'
+                return 'bg-amber-700/50 text-amber-700'
             case 'orange':
-                return 'bg-orange-300/50'
+                return 'bg-orange-300/50 text-orange-500'
             default:
-                return 'bg-slate-300/50'
+                return 'bg-slate-300/50 text-slate-500'
         }
     })()
-    return <span className={['p-1 rounded-sm', bg].join(' ')}>{name}</span>
+    return <span className={['rounded-sm p-1', bg].join(' ')}>{name}</span>
 }
 
 export default Tag
