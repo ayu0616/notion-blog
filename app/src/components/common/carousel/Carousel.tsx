3c631e197b3f78ef9b2108120047b7579c0cf1c2
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-18 12:34:15 +0900

    ボタンをクリックしたらスクロールする機能を追加した

--- app/src/components/common/carousel/Carousel.tsx
+++ app/src/components/common/carousel/Carousel.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa'
 import Image from '../ImageBase'
 import './Carousel.style.scss'
@@ -7,18 +8,49 @@ interface CarouselProps {
 }
 
 const Carousel = ({ srcList, ...props }: CarouselProps) => {
+    const container = useRef<HTMLDivElement>(null)
+
+    const goNext = () => {
+        if (!container.current) {
+            return
+        }
+        container.current.scrollBy({
+            left: container.current.clientWidth,
+            behavior: 'smooth',
+        })
+    }
+
+    const goPrev = () => {
+        if (!container.current) {
+            return
+        }
+        container.current.scrollBy({
+            left: -container.current.clientWidth,
+            behavior: 'smooth',
+        })
+    }
+
     return (
         <div className='relative'>
             {/* 進む・戻るボタン */}
-            <button className='absolute left-0 top-0 z-10 h-full text-white/50 hover:text-white/75 hover:bg-black/50 bg-black/25 flex justify-center items-center text-2xl px-2 md:px-4'>
+            <button
+                className='absolute left-0 top-0 z-10 flex h-full items-center justify-center bg-black/25 px-2 text-2xl text-white/50 hover:bg-black/50 hover:text-white/75 md:px-4'
+                onClick={goPrev}
+            >
                 <FaAngleLeft></FaAngleLeft>
             </button>
-            <button className='absolute right-0 top-0 z-10 h-full text-white/50 hover:text-white/75 hover:bg-black/50 bg-black/25 flex justify-center items-center text-2xl px-2 md:px-4'>
+            <button
+                className='absolute right-0 top-0 z-10 flex h-full items-center justify-center bg-black/25 px-2 text-2xl text-white/50 hover:bg-black/50 hover:text-white/75 md:px-4'
+                onClick={goNext}
+            >
                 <FaAngleRight></FaAngleRight>
             </button>
 
             {/* 画像 */}
-            <div className='carousel-container flex aspect-video w-full snap-x snap-mandatory overflow-x-scroll'>
+            <div
+                className='carousel-container flex aspect-video w-full snap-x snap-mandatory overflow-x-scroll'
+                ref={container}
+            >
                 {srcList.map((src, index) => {
                     return (
                         <Image
