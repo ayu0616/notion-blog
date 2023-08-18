e38b9d48d9476ac687eaee101b5b2dd32a0ffc4c
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-18 13:50:08 +0900

    string型も受け取れるように変更した

--- app/src/util/dateToStr.ts
+++ app/src/util/dateToStr.ts
@@ -1,3 +1,4 @@
-export default function dateToStr(date: Date): string {
+export default function dateToStr(date: Date | string): string {
+    if (typeof date === 'string') date = new Date(date)
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
 }
