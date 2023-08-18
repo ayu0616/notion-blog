87e591f964b640f02ebcd30cbb152635a2969d1c
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-18 21:57:30 +0900

    ホットリロードする設定を追加した

--- app/next.config.js
+++ app/next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
     images: {
         remotePatterns: [{ hostname: '*' }],
     },
+    webpack: (config, context) => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        }
+        return config
+    },
 }
 
 module.exports = nextConfig
