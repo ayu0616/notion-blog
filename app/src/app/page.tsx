d3d292cfa33b093b05cb9e6f0ffb1ab643f6660b
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-19 10:15:56 +0900

    データをAPI経由で取得するようにした

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -6,36 +6,11 @@ import { Page } from '@/type/page/page'
 import dateToStr from '@/util/dateToStr'
 import Link from 'next/link'
 
-export default function Home() {
-    const mockData: Page[] = [
-        {
-            id: '9b548af9-e899-4002-8202-613be38bceac',
-            title: 'Notionで書いたブログをウェブページとして公開してみた【Next.js】',
-            tags: [{ name: 'プログラミング', color: 'blue' }],
-            lastEditedTime: '2023-08-11T07:10:00.000Z',
-            slug: 'notion-blog-nextjs',
-            status: 'writing',
-            publishDate: null,
-            blocks: [],
-            image: null,
-            description:
-                'Notionのページをブログとして公開したいと思ったのでNext.jsを利用して自作することにしました',
-        },
-        {
-            id: 'f803988f-6182-4d9b-a688-37a463669ef0',
-            title: 'テストページ',
-            tags: [{ name: 'プログラミング', color: 'blue' }],
-            lastEditedTime: '2023-08-11T02:00:00.000Z',
-            slug: 'test',
-            status: 'published',
-            publishDate: '2023-08-11',
-            blocks: [],
-            image: 'https://4.bp.blogspot.com/-NpzSXawlkuQ/WaPwHY3hy_I/AAAAAAABGR8/VjeNZ-J7RdYJmhcEecLUY9q9_GMERe0NACLcBGAs/s800/test_print_happy_schoolgirl.png',
-            description:
-                'これはテストです。これはテストです。これはテストです。これはテストです。これはテストです。これはテストです。これはテストです。',
-        },
-    ]
-    const carouselData = mockData.map((page) => {
+export default async function Home() {
+    const { pageData }: { pageData: Page[] } = await fetch(
+        'http://localhost:3000/api/page-data',
+    ).then((res) => res.json())
+    const carouselData = pageData.map((page) => {
         return {
             image: page.image ?? '/no_image.jpg',
             publishDate: page.publishDate ?? '1970-01-01',
@@ -48,7 +23,7 @@ export default function Home() {
         <main className='mx-auto max-w-4xl space-y-4'>
             <Carousel data={carouselData}></Carousel>
             <div className='grid gap-4 px-4'>
-                {mockData.map((page, i) => {
+                {pageData.map((page, i) => {
                     return (
                         <Link href={`/blog/${page.slug}`} key={i}>
                             <Card horizontal className=''>
