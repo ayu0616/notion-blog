0c9abdab1be624365b30e30cbf320cab108437b2
ayu0616 <ayumuogawa0616@gmail.com>
2023-08-20 11:02:23 +0900

    名前とSNSアイコンを配置した

--- app/src/components/common/profile/Profile.tsx
+++ app/src/components/common/profile/Profile.tsx
@@ -1,7 +1,54 @@
+import { FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa'
+
 interface ProfileProps {}
 
 const Profile = ({ ...props }: ProfileProps) => {
-    return <div></div>
+    const snsIcons = [
+        {
+            name: 'twitter',
+            icon: <FaTwitter></FaTwitter>,
+            link: 'https://twitter.com/hassaku_0616',
+            color: 'bg-blue-500 text-white hover:bg-blue-400',
+        },
+        {
+            name: 'instagram',
+            icon: <FaInstagram></FaInstagram>,
+            link: 'https://instagram.com/hassaku_0616',
+            color: 'bg-pink-500 text-white hover:bg-pink-400',
+        },
+        {
+            name: 'github',
+            icon: <FaGithub></FaGithub>,
+            link: 'https://github.com/ayu0616',
+            color: 'bg-gray-600 text-white hover:bg-gray-500',
+        },
+    ]
+    return (
+        <div>
+            <h4>はっさく</h4>
+            <div className='flex'>
+                {snsIcons.map((snsIcon) => {
+                    return (
+                        <a
+                            key={snsIcon.name}
+                            href={snsIcon.link}
+                            target='_blank'
+                            rel='noreferrer'
+                            className='mr-2'
+                        >
+                            <div
+                                className={['rounded-full p-2', snsIcon.color].join(
+                                    ' ',
+                                )}
+                            >
+                                {snsIcon.icon}
+                            </div>
+                        </a>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default Profile
