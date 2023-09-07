0e95f345e89f26ecca0d2f0eb12efd9a23268fbc
ayu0616 <ayumuogawa0616@gmail.com>
2023-09-08 08:24:40 +0900

    add: ページの全タグを取得する関数を作成した

---
+++ app/src/app/BlogPages.tsx
@@ -0,0 +1,79 @@
+'use client'
+
+import Card from '@/components/common/Card'
+import Image from '@/components/common/ImageBase'
+import Checkbox, { CheckboxProps } from '@/components/common/checkbox/Checkbox'
+import TagList from '@/components/common/tag/TagList'
+import { Page } from '@/type/page/page'
+import dateToStr from '@/util/dateToStr'
+import Link from 'next/link'
+import { useState } from 'react'
+
+interface BlogPageProps {
+    pageData: Page[]
+}
+
+/** ブログのページ一覧とタグによる検索を表示するコンポーネント */
+const BlogPages = ({ pageData, ...props }: BlogPageProps) => {
+    const [showPageData, setPageData] = useState<Page[]>(pageData)
+    const tagData = getTagData(pageData)
+    return (
+        <>
+            <div className='flex gap-2 px-4'>
+                {tagData.map((tag, i) => {
+                    return <Checkbox {...tag} key={i} />
+                })}
+            </div>
+            <div className='grid gap-4 px-4'>
+                {showPageData.map((page, i) => {
+                    return (
+                        <Link href={`/blog/${page.slug}`} key={i}>
+                            <Card horizontal className=''>
+                                <Image
+                                    src={page.image ?? '/no_image.jpg'}
+                                    alt=''
+                                    className='w-1/3 rounded-l-md'
+                                    objectFit='cover'
+                                ></Image>
+                                <div className='flex flex-1 flex-col gap-2 p-3'>
+                                    <h3>{page.title}</h3>
+                                    <TagList
+                                        isLink
+                                        tagData={page.tags}
+                                    ></TagList>
+                                    <p className='text-end text-sm text-gray-600'>
+                                        {dateToStr(
+                                            new Date(page.publishDate ?? 0),
+                                        )}
+                                    </p>
+                                    <p>{page.description}</p>
+                                </div>
+                            </Card>
+                        </Link>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
+
+export default BlogPages
+
+/** ページにふされているすべてのタグの取得する */
+const getTagData = (pageData: Page[]): CheckboxProps[] => {
+    const tagSet = new Set<string>()
+    pageData.forEach((page) => {
+        page.tags.forEach((tag) => {
+            tagSet.add(JSON.stringify(tag))
+        })
+    })
+    const tagData: CheckboxProps[] = []
+    tagSet.forEach((tag) => {
+        const t = JSON.parse(tag)
+        tagData.push({
+            value: t.name,
+            color: t.color,
+        })
+    })
+    return tagData
+}
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,12 +1,7 @@
-import Card from '@/components/common/Card'
-import Image from '@/components/common/ImageBase'
 import Carousel from '@/components/common/carousel/Carousel'
-import Checkbox, { CheckboxProps } from '@/components/common/checkbox/Checkbox'
-import TagList from '@/components/common/tag/TagList'
 import { Page } from '@/type/page/page'
-import dateToStr from '@/util/dateToStr'
 import fs from 'fs'
-import Link from 'next/link'
+import BlogPages from './BlogPages'
 
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL
 
@@ -44,64 +39,10 @@ export default async function Home() {
         }
     })
 
-    const tagData = getTagData(pageData)
-
     return (
         <main className='mx-auto max-w-4xl space-y-4 pb-4'>
             <Carousel data={carouselData}></Carousel>
-            <div className='flex gap-2 px-4'>
-                {tagData.map((tag, i) => {
-                    return <Checkbox {...tag} key={i} />
-                })}
-            </div>
-            <div className='grid gap-4 px-4'>
-                {pageData.map((page, i) => {
-                    return (
-                        <Link href={`/blog/${page.slug}`} key={i}>
-                            <Card horizontal className=''>
-                                <Image
-                                    src={page.image ?? '/no_image.jpg'}
-                                    alt=''
-                                    className='w-1/3 rounded-l-md'
-                                    objectFit='cover'
-                                ></Image>
-                                <div className='flex flex-1 flex-col gap-2 p-3'>
-                                    <h3>{page.title}</h3>
-                                    <TagList
-                                        isLink
-                                        tagData={page.tags}
-                                    ></TagList>
-                                    <p className='text-end text-sm text-gray-600'>
-                                        {dateToStr(
-                                            new Date(page.publishDate ?? 0),
-                                        )}
-                                    </p>
-                                    <p>{page.description}</p>
-                                </div>
-                            </Card>
-                        </Link>
-                    )
-                })}
-            </div>
+            <BlogPages pageData={pageData} />
         </main>
     )
-}
-
-/** ページにふされているすべてのタグの取得する */
-const getTagData = (pageData: Page[]): CheckboxProps[] => {
-    const tagSet = new Set<string>()
-    pageData.forEach((page) => {
-        page.tags.forEach((tag) => {
-            tagSet.add(JSON.stringify(tag))
-        })
-    })
-    const tagData: CheckboxProps[] = []
-    tagSet.forEach((tag) => {
-        const t = JSON.parse(tag)
-        tagData.push({
-            value: t.name,
-            color: t.color,
-        })
-    })
-    return tagData
 }
