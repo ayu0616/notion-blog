786ab7955089a25a221e130430dc35ec3d64f350
ayu0616 <ayumuogawa0616@gmail.com>
2023-09-20 23:25:37 +0900

    add: 属性を追加した

--- app/src/components/common/checkbox/Checkbox.stories.tsx
+++ app/src/components/common/checkbox/Checkbox.stories.tsx
@@ -20,5 +20,8 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-    args: {},
+    args: {
+        value: 'Checkbox',
+        name: 'checkbox',
+    },
 }
