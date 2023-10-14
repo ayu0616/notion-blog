6a78bd744f43f682a30bbe082c92d70ee0a10dfd
ayu0616 <ayumuogawa0616@gmail.com>
2023-10-14 10:54:16 +0900

    delete: コメントを削除した

--- app/src/components/accordion/AccordionButton.tsx
+++ app/src/components/accordion/AccordionButton.tsx
@@ -6,14 +6,11 @@ interface AccordionButtonProps {
 }
 
 const AccordionButton = ({ open = false, ...props }: AccordionButtonProps) => {
-    // const [isOpen, setOpen] = useState(open)
-
     return (
         <div
             {...props}
             className='accordion-button group cursor-pointer select-none rounded-md bg-white transition-all duration-300 ease-in-out hover:bg-slate-50 active:bg-slate-100 data-[open="true"]:rounded-b-none data-[open="true"]:border-b'
             data-open={open}
-            // onClick={() => setOpen((p) => !p)}
         ></div>
     )
 }
