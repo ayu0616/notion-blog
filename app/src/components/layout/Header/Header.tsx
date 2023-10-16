6c7ee27319a29ac9e84f04538e2c597dd901f474
ayu0616 <ayumuogawa0616@gmail.com>
2023-10-16 10:39:05 +0900

    feat: Accordionを導入した

--- app/src/components/layout/Header/Header.tsx
+++ app/src/components/layout/Header/Header.tsx
@@ -1,9 +1,12 @@
-'use client'
-
 import Link from 'next/link'
-import { useEffect, useRef, useState } from 'react'
-import { BsList, BsXLg } from 'react-icons/bs'
+import { BsList, BsX } from 'react-icons/bs'
 import { FaFile, FaHome, FaList } from 'react-icons/fa'
+
+import {
+    Accordion,
+    AccordionButton,
+    AccordionContent,
+} from '@/components/common/Accordion'
 
 interface MenuItem {
     icon: JSX.Element
@@ -12,10 +15,6 @@ interface MenuItem {
 }
 
 const Header = () => {
-    const [open, setOpen] = useState(false)
-    const navElem = useRef<HTMLDivElement>(null)
-    const ulElem = useRef<HTMLUListElement>(null)
-
     const menu: MenuItem[] = [
         {
             icon: <FaHome />,
@@ -31,55 +30,50 @@ const Header = () => {
             href: '/tag',
         },
     ]
-    const navHeight = 45 * menu.length
-
-    const toggleOpen = () => setOpen((prev) => !prev)
-
-    useEffect(() => {
-        if (open) {
-            navElem.current?.style.setProperty('height', `${navHeight}px`)
-            setTimeout(() => {
-                ulElem.current?.classList.remove('hidden')
-            }, 250)
-        } else {
-            navElem.current?.style.setProperty('height', '0')
-            ulElem.current?.classList.add('hidden')
-        }
-    }, [open, navHeight])
 
     return (
         <header className='sticky left-0 top-0 z-50 w-full items-center justify-between bg-orange-500 text-white md:flex'>
-            <div className='flex items-center justify-between'>
-                <h1 className='px-6 py-4 text-2xl'>
-                    <Link href='/'>はっさくの旅ブログ</Link>
-                </h1>
-                <div
-                    className='p-3 text-4xl hover:bg-orange-600 active:bg-orange-700 md:hidden'
-                    onClick={toggleOpen}
-                >
-                    {open ? <BsXLg /> : <BsList />}
-                </div>
-            </div>
-            <nav
-                ref={navElem}
-                className='transition-[height] duration-500 ease-in-out md:block md:h-[auto!important]'
-            >
-                <ul className='md:flex md:pr-3' ref={ulElem}>
-                    {menu.map((item, index) => (
-                        <li
-                            key={index}
-                            className='border-t border-white hover:bg-orange-600 active:bg-orange-700 md:border-t-0'
-                        >
-                            <Link href={item.href ?? '/'}>
-                                <div className='flex items-center gap-1 px-6 py-3 md:px-3'>
-                                    {item.icon}
-                                    {item.title}
+            <Accordion variant='unstyled'>
+                <div className='flex items-center justify-between'>
+                    <h1 className='px-6 py-4 text-2xl'>
+                        <Link href='/'>はっさくの旅ブログ</Link>
+                    </h1>
+                    <AccordionButton>
+                        <div className='p-2 text-4xl md:hidden'>
+                            <div className='relative rounded-full bg-orange-500 p-1 hover:bg-orange-600 active:bg-orange-700'>
+                                <div className='opacity-0'>
+                                    <BsX />
                                 </div>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+                                <div className='absolute-center opacity-0 transition-opacity duration-300 group-data-[open="true"]:opacity-100'>
+                                    <BsX />
+                                </div>
+                                <div className='absolute-center opacity-0 transition-opacity duration-300 group-data-[open="false"]:opacity-100'>
+                                    <BsList />
+                                </div>
+                            </div>
+                        </div>
+                    </AccordionButton>
+                </div>
+                <AccordionContent>
+                    <nav className='bg-orange-500 md:block md:h-[auto!important]'>
+                        <ul className='md:flex md:pr-3'>
+                            {menu.map((item, index) => (
+                                <li
+                                    key={index}
+                                    className='border-t border-white hover:bg-orange-600 active:bg-orange-700 md:border-t-0'
+                                >
+                                    <Link href={item.href ?? '/'}>
+                                        <div className='flex items-center gap-1 px-6 py-3 md:px-3'>
+                                            {item.icon}
+                                            {item.title}
+                                        </div>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </AccordionContent>
+            </Accordion>
         </header>
     )
 }
