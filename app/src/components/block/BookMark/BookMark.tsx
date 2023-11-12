b69b150ce79c5348968418221e44a0bab6989cb5
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-12 16:39:50 +0900

    fix: テキストの見た目を変更した

--- app/src/components/block/BookMark/BookMark.tsx
+++ app/src/components/block/BookMark/BookMark.tsx
@@ -13,16 +13,16 @@ const BookMark = async ({ href, ...props }: BookMarkProps) => {
             <a href={href} target='_blank' rel='noopener noreferrer'>
                 <div className='space-y-1 p-3'>
                     <h6>{title}</h6>
-                    <p className='text-xs leading-3 md:text-sm'>
+                    <p className='text-xs leading-3 text-gray-500 md:text-sm'>
                         {description}
                     </p>
-                    <p className='flex items-center justify-end text-right text-xs text-gray-500 md:text-sm'>
+                    <p className='text-xs-1 flex items-center justify-end text-right md:text-sm'>
                         <ImageBase
                             src={iconUrl}
                             alt={`icon of ${href}`}
                             className='mr-1 inline-block h-[12px] w-[12px] md:mr-2 md:h-4 md:w-4'
                         />
-                        {href}
+                        <p className='line-clamp-1 flex-1'>{href}</p>
                     </p>
                 </div>
             </a>
