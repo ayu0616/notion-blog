c6d94b1bf915c5972994194bf7546feca2a04c63
ayu0616 <ayumuogawa0616@gmail.com>
2023-11-14 09:39:56 +0900

    fix: auto format

--- app/src/util/dateToStr.ts
+++ app/src/util/dateToStr.ts
@@ -1,7 +1,7 @@
 export default function dateToStr(date: Date | string): string {
     const _date = new Date(date)
-    const y = ('000' + _date.getFullYear().toString()).slice(-4)
-    const m = ('0' + (_date.getMonth() + 1).toString()).slice(-2)
-    const d = ('0' + _date.getDate().toString()).slice(-2)
+    const y = `000${_date.getFullYear().toString()}`.slice(-4)
+    const m = `0${(_date.getMonth() + 1).toString()}`.slice(-2)
+    const d = `0${_date.getDate().toString()}`.slice(-2)
     return `${y}-${m}-${d}`
 }
