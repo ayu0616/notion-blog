595f8f8d76c5668c0349d638db14b9a2b2ae028a
ayu0616 <ayumuogawa0616@gmail.com>
2023-12-30 14:26:12 +0900

    fix: テーブルが存在するときにエラーを無視してプログラムを終了させるようにした

--- backend/db/create_table.py
+++ backend/db/create_table.py
@@ -1,11 +1,16 @@
 from dynamodb import dynamodb
 from settings import TABLE_NAME
 
-dynamodb.create_table(
-    AttributeDefinitions=[
-        {"AttributeName": "slug", "AttributeType": "S"},
-    ],
-    TableName=TABLE_NAME,
-    KeySchema=[{"AttributeName": "slug", "KeyType": "HASH"}],
-    ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
-)
+if __name__ == "__main__":
+    try:
+        dynamodb.create_table(
+            AttributeDefinitions=[
+                {"AttributeName": "slug", "AttributeType": "S"},
+            ],
+            TableName=TABLE_NAME,
+            KeySchema=[{"AttributeName": "slug", "KeyType": "HASH"}],
+            ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+        )
+        print("Table created successfully")
+    except dynamodb.meta.client.exceptions.ResourceInUseException:
+        print("Table already exists")
